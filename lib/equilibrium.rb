74886f75b117eda41e965c3602b5d3af586b18b0
cyberplanner <sal@salweb.co.uk>
2016-12-09 22:21:29 +0000

    :tada: First test is passing.

--- lib/equilibrium.rb
+++ lib/equilibrium.rb
@@ -1,6 +1,9 @@
 class Equilibrium
+
   def solution(array)
+    array.each_index.select do |i|
+      array[0...i].inject(0, :+) == array[i+1..-1].inject(0, :+)
+    end
   end
-
-
+  
 end
--- spec/equilibrium_spec.rb
+++ spec/equilibrium_spec.rb
@@ -6,5 +6,10 @@ describe Equilibrium do
     expect(equilibrium).to respond_to(:solution).with(1).argument
   end
 
-
+  describe "#solution" do
+    it "returns equilibrium index of the given array" do
+      arr1 = [-1, 3, -4, 5, 1, -6, 2, 1]
+      expect(equilibrium.solution(arr1)).to eq [1, 3, 7]
+    end
+  end
 end
